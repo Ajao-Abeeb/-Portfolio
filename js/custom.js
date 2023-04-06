305ded7b8c9f8d3b81ca0acaf34cae409cf98fb7
Ajao-Abeeb <abeebajao526@gmail.com>
2023-04-06 14:50:15 +0100

    theme change

--- js/custom.js
+++ js/custom.js
@@ -13,12 +13,4 @@
  
 
 })(jQuery);
-function darkmode(){
-  var dark = document.getElementById('dark');
-   var element = document.body;
-  element.classList.toggle("darkmode");
-  dark.style.color = "white";
-  
-  return ;
-  
-}
+ 
